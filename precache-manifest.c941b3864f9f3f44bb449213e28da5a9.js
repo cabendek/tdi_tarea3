1eb5f6b3c87cbf119b98fd2c06cfe7e3e917f238
MAC <mac@MacBook-Pro-de-MAC.local>
2021-05-24 01:18:27 -0400

    Updates

---
+++ precache-manifest.c941b3864f9f3f44bb449213e28da5a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "664ae909a076630074efe43a4c24c706",
+    "url": "/tdi_tarea3/index.html"
+  },
+  {
+    "revision": "79cf9d4cfde7b3cbfd22",
+    "url": "/tdi_tarea3/static/css/2.36b72570.chunk.css"
+  },
+  {
+    "revision": "95093a9fa08b185a3900",
+    "url": "/tdi_tarea3/static/css/main.69b21db8.chunk.css"
+  },
+  {
+    "revision": "79cf9d4cfde7b3cbfd22",
+    "url": "/tdi_tarea3/static/js/2.e69fd042.chunk.js"
+  },
+  {
+    "revision": "34a6791198b4fc3bb1dac4b05d6d1c86",
+    "url": "/tdi_tarea3/static/js/2.e69fd042.chunk.js.LICENSE"
+  },
+  {
+    "revision": "95093a9fa08b185a3900",
+    "url": "/tdi_tarea3/static/js/main.59e94b3e.chunk.js"
+  },
+  {
+    "revision": "e800ff3094bfa6ee5475",
+    "url": "/tdi_tarea3/static/js/runtime-main.90b6d3a0.js"
+  },
+  {
+    "revision": "d8a5c98e52b1dab9b9d04947ce1fb0f0",
+    "url": "/tdi_tarea3/static/media/paper-plane2.d8a5c98e.svg"
+  }
+]);
